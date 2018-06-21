c670ebbb7bc015624f85182744cc41f6ad14ad8a
shoko.nagata <shoko.nagata@rakuten.com>
2018-06-21 03:59:31 +0000

    teisei

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,25 @@
 @section('content')
     @if (Auth::check())
         <div class="row">
-            <aside class="col-md-4">
+            <aside class="col-xs-4">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h3 class="panel-title">{{ $user->name }}</h3>
+                    </div>
+                    <div class="panel-body">
+                        <img class="media-object img-rounded img-responsive" src="{{ Gravatar::src($user->email, 500) }}" alt="">
+                    </div>
+                </div>
+                @if (Auth::id() == $user->id)
+                  {!! Form::open(['route' => 'microposts.store']) !!}
+                      <div class="form-group">
+                          {!! Form::textarea('content', old('content'), ['class' => 'form-control', 'rows' => '2']) !!}
+                          {!! Form::submit('Post', ['class' => 'btn btn-primary btn-block']) !!}
+                      </div>
+                  {!! Form::close() !!}
+            @endif
             </aside>
+            
             <div class="col-xs-8">
                 @if (count($microposts) > 0)
                     @include('microposts.microposts', ['microposts' => $microposts])
